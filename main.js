d748e85ab00a39835ab881720bff9e95bf26eb7b
leesaswift <leesaswift@gmail.com>
2023-11-20 20:26:44 -0500

    working on audio

--- main.js
+++ main.js
@@ -1,54 +1,57 @@
-const iTunesUrl = "https://itunes.apple.com/search?term=";
-const limit = "&limit=20";
+
+const musicURL = "https://itunes.apple.com/search?term=";
 let searchForm = document.getElementById("searchForm");
-let searchTerm = document.getElementById("searchField");
+let searchField = document.getElementById("searchField");
 let button = document.getElementById("searchButton");
-let display = document.getElementById("display");
-let preview = document.getElementById("preview");
-let previewButton = document.getElementById("previewButton")
-searchForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    let term = searchTerm.value;
-    console.log({term});
-    fetch(iTunesUrl + term + limit, {
-        method: "Get",
-        headers: {"Content-Type": "application/json"},
-}).then((response) => {
-    if (response.status === 200) {
-        return response.json();
-    } else {
-        throw new Error("No results found")
-    }
-}).then((parsedJsonResponse) => {
-    console.log(parsedJsonResponse);
-    const songClips = parsedJsonResponse.results;
-    display.innerHTML = ""
-    if (songClips.length === 0) {
-        let errorMsg = document.createElement("h2")
-        errorMsg.innerText = "No results found"
-        display.appendChild(errorMsg)
-    }
-    return songClips.map(results => {
-        preview.innerHTML = `
-        <figure>
-            <figcaption id="previewTrackName">Preview:</figcaption>
-            <audio controls src=""></audio>
-            </figure>`,
-            display.innerHTML += `
-            <div class="songTitle">
-                <img src=${results.artworkUrl100} />
-                <button class="previewButton" data-id=${results.trackID}>${results.trackName}</button>
-                <h3>${results.artistName}</h3>
-                </div>
-            `
-            // // document.querySelectorAll(".previewButton").forEach(button =>{
-            // //     button.addEventListener("click",function(){
-            // //         const previewUrl = this.getAttribute("data-preview-Url")
-            // //         const audioPlayer = document.getElementById("audioPlayer")
-            // //         audioPlayer.src = previewUrl
-            // //         audioPlayer.play()
-            //     })
-            // })
-    })
+let resultsContainer = document.getElementById("results");
+let resultsCard = document.querySelectorAll(".resultsCard");
+let playingPreview = document.getElementById("playingPreview");
+
+
+
+
+    searchForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        let field = []
+         field = searchField.value
+   /*  console.log(field); */
+    fetch(musicURL + field + "&media=music").then((response) => {
+        if (response.status === 200) {
+            return response.json();}
+            
+        }).then((parsedJsonResponse) => {
+            console.log(parsedJsonResponse);
+            const songs = parsedJsonResponse.results;
+            results.innerHTML = []
+            return songs.map(results => {
+                const resultsCard = document.createElement('div');
+                artist = document.createElement('h3');
+                trackList = document.createElement('p');
+                album = document.createElement('img');
+                let playButton = document.createElement('button');
+
+                playButton.innerText = "Play Sample";
+
+                playButton.addEventListener('click', (event) => {
+                    event.preventDefault();
+                    playingPreview.src = results.previewUrl
+                });
+                
+                
+                artist.innerHTML = results.artistName;
+                trackList.innerHTML = results.trackName;
+                album.src = results.artworkUrl100;
+
+                resultsCard.appendChild(album);
+                resultsCard.appendChild(artist);
+                resultsCard.appendChild(trackList);
+                resultsCard.appendChild(playButton);
+                
+                resultsContainer.appendChild(resultsCard);
+                
+                
+    });
+   
+    
 })
 })
