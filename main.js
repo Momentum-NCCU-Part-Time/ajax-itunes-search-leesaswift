73f37279db0d2c991519cca5626027c2dbf224cb
Leesa Swift <145400567+leesaswift@users.noreply.github.com>
2023-11-22 02:02:59 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,57 +1,87 @@
-
-const musicURL = "https://itunes.apple.com/search?term=";
+const iTunesUrl = "https://itunes.apple.com/search?term=";
 let searchForm = document.getElementById("searchForm");
-let searchField = document.getElementById("searchField");
+let searchTerm = document.getElementById("searchField");
 let button = document.getElementById("searchButton");
-let resultsContainer = document.getElementById("results");
+let display = document.getElementById("display");
+// let preview = document.getElementById("preview");
 let resultsCard = document.querySelectorAll(".resultsCard");
-let playingPreview = document.getElementById("playingPreview");
-
-
+let previewButton = document.getElementById("previewButton");
 
 
     searchForm.addEventListener('submit', (event) => {
-        event.preventDefault();
-        let field = []
-         field = searchField.value
-   /*  console.log(field); */
-    fetch(musicURL + field + "&media=music").then((response) => {
-        if (response.status === 200) {
-            return response.json();}
+    event.preventDefault();
+    let term = []
+    term = searchTerm.value 
+    console.log({term});
+
+    fetch(iTunesUrl + term + "&media=music").then((response) => {
+    if (response.status === 200) {
+        return response.json();
+    } else {
+        throw new Error("No results found")
+    }
+
+    }).then((parsedJsonResponse) => {
+    console.log(parsedJsonResponse);
+    const songs = parsedJsonResponse.display;
+    display.innerHTML = []
+    // if (songClips.length === 0) {
+    //     let errorMsg = document.createElement("h2")
+    //     errorMsg.innerText = "No results found"
+    //     display.appendChild(errorMsg)
+    // }
+
+
+        return songs?.map(display => {
+            const resultsCard = document.createElement('div');
+            artist = document.createElement('h3');
+            trackList = document.createElement('p');
+            album = document.createElement('img');
+            let audioPlay = document.createElement('button');
+
+            audioPlay.innerText = "Play Preview";
+
+            audioPlay.addEventListener('click', (event) => {
+                event.preventDefault();
+                previewButton.src = display.previewUrl
+            });
+
+            artist.innerHTML = display.artistName;
+            trackList.innerHTML = display.trackName;
+            ablbum.src = display.artworkUrl100;
+
+            resultsCard.appendChild(album);
+            resultsCard.appendChild(artist);
+            resultsCard.appendChild(trackList);
+            resultsCard.appendChild(playButton);
+
+            results.appendChild(resultsCard);
+
+        
             
-        }).then((parsedJsonResponse) => {
-            console.log(parsedJsonResponse);
-            const songs = parsedJsonResponse.results;
-            results.innerHTML = []
-            return songs.map(results => {
-                const resultsCard = document.createElement('div');
-                artist = document.createElement('h3');
-                trackList = document.createElement('p');
-                album = document.createElement('img');
-                let playButton = document.createElement('button');
-
-                playButton.innerText = "Play Sample";
-
-                playButton.addEventListener('click', (event) => {
-                    event.preventDefault();
-                    playingPreview.src = results.previewUrl
-                });
-                
-                
-                artist.innerHTML = results.artistName;
-                trackList.innerHTML = results.trackName;
-                album.src = results.artworkUrl100;
-
-                resultsCard.appendChild(album);
-                resultsCard.appendChild(artist);
-                resultsCard.appendChild(trackList);
-                resultsCard.appendChild(playButton);
-                
-                resultsContainer.appendChild(resultsCard);
-                
-                
-    });
-   
-    
+        });
 })
 })
+
+    // return songClips.map(results => {
+    //     preview.innerHTML = `
+    //     <figure>
+    //         <figcaption id="previewTrackName">Preview:</figcaption>
+    //         <audio controls src=""></audio>
+    //         </figure>`,
+    //         display.innerHTML += `
+    //         <div class="songTitle">
+    //             <img src=${results.artworkUrl100} />
+    //             <button class="previewButton" data-id=${results.trackID}>${results.trackName}</button>
+    //             <h3>${results.artistName}</h3>
+    //             </div>
+    //         `
+            // // document.querySelectorAll(".previewButton").forEach(button =>{
+            // //     button.addEventListener("click",function(){
+            // //         const previewUrl = this.getAttribute("data-preview-Url")
+            // //         const audioPlayer = document.getElementById("audioPlayer")
+            // //         audioPlayer.src = previewUrl
+            // //         audioPlayer.play()
+            //     })
+            // })
+
